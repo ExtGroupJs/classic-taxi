e49a389efcc4ef0c12025bc468d4711777140afe
duniesky <dunieskydp86@gmail.com>
2025-03-27 12:17:36 -0400

    div head

---
+++ static/assets/classic/js/car-selection.js
@@ -0,0 +1,24 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  
+  const headDiv = document.getElementById('headdiv');
+  const headName = document.getElementById('headname');
+  const headDescription = document.getElementById('headdescription');
+
+  axios.get('/business-gestion/cars/')
+    .then(response => {     
+      const cars = response.data.results;
+      if (cars.length === 0) return;
+
+      // Seleccionar un coche aleatoriamente
+      const randomCar = cars[Math.floor(Math.random() * cars.length)];
+      // Actualizar el contenido de la página
+      headDiv.style.backgroundImage = `url('${randomCar.main_picture}')`;
+      headName.textContent = `${randomCar.year} ${randomCar.model}`;
+      headDescription.textContent = randomCar.extra_info;
+    })
+    .catch(error => {
+      console.error('Error fetching car data:', error);
+    });
+}); 
